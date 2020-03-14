21f9210ea04c1568b15446981b9db46c5db4848a
eduardotbezerra <profissional.edu.teixeira@gmail.com>
2020-03-14 15:26:00 -0300

    add top-menu css and js

---
+++ js/top-menu.js
@@ -0,0 +1,53 @@
+function topnav_success() {
+  var x = document.getElementById("topnav-success");
+  if (x.className === "topnav-success") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-success";
+  }
+}
+
+function topnav_danger() {
+  var x = document.getElementById("topnav-danger");
+  if (x.className === "topnav-danger") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-danger";
+  }
+}
+
+function topnav_warning() {
+  var x = document.getElementById("topnav-warning");
+  if (x.className === "topnav-warning") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-warning";
+  }
+}
+
+function topnav_info() {
+  var x = document.getElementById("topnav-info");
+  if (x.className === "topnav-info") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-info";
+  }
+}
+
+function topnav_light() {
+  var x = document.getElementById("topnav-light");
+  if (x.className === "topnav-light") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-light";
+  }
+}
+
+function topnav_dark() {
+  var x = document.getElementById("topnav-dark");
+  if (x.className === "topnav-dark") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav-dark";
+  }
+}
